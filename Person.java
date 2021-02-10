b9de73bb10adabfb9d2f6a4a6aa0f1ed0bced32d
unknown <marcus.larsen.swe@gmail.com>
2021-02-10 01:38:08 +0100

    object change with stream 7

--- Person.java
+++ Person.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Person extends Npc implements Runnable {
     String xname;
@@ -6,6 +8,7 @@ public class Person extends Npc implements Runnable {
     Inventory iTest;
     int currentrum;
     Inventory npcItems;
+    GameObject[] showItems;
 
     public Person(String xname, int y, Inventory o, int x){
         this.xname = xname;
@@ -17,10 +20,16 @@ public class Person extends Npc implements Runnable {
 
     @Override
     public void run(){
-        //iTest.setInt(this.value);
+        boolean pickUpItemOrNot = (Math.random() < 0.5);
+
+
         leaveRoom();
         goToRoom();
 
+        showItems();
+        if(pickUpItemOrNot && this.showItems.length < 2) {
+        getRoomItem();
+        }
     }
 
     public void goToRoom(){
@@ -63,8 +72,68 @@ public class Person extends Npc implements Runnable {
         }
     }
 
+
+
+    public void addRoomItem(GameObject t){
+        Stream<GameObject> myStreamX = Stream.of(this.npcItems.xObjects);
+        this.npcItems.xObjects = myStreamX
+                .map(x -> {if(x.getItemName() == Game.emptyCell.getItemName()){
+                    return t;
+                } else return x;
+                })
+                .distinct()
+                .collect(Collectors.toList()).toArray(new GameObject[2]);
+
+        this.npcItems.xObjects = myStreamX
+                .map(x -> {if(x == null){
+                    return Game.emptyCell;
+                } else return x;
+                })
+                .collect(Collectors.toList()).toArray(new GameObject[2]);
+    }
+
+
+
     public void defaultFillInventory(){
         Arrays.fill(this.npcItems.xObjects, Game.emptyCell);
+    }
+
+    public void getRoomItem(){
+        switch(this.currentrum){
+            case 1:
+                this.addRoomItem(Game.rum1.getItemNPC());
+                break;
+            case 2:
+                this.addRoomItem(Game.rum2.getItemNPC());
+                break;
+            case 3:
+                this.addRoomItem(Game.rum3.getItemNPC());
+                break;
+            case 4:
+                this.addRoomItem(Game.rum4.getItemNPC());
+                break;
+        }
+    }
+
+    public synchronized void dropNPCItem(GameObject d){
+        Stream<GameObject> myStreamX23 = Stream.of(this.npcItems.xObjects);
+        this.npcItems.xObjects = myStreamX23
+                .map(x -> {if(x.getItemName() == d.getItemName()){
+                    return Game.emptyCell;
+                } else return x;
+                })
+                .collect(Collectors.toList()).toArray(new GameObject[0]);
+    }
+
+    public int showItems(){
+        Stream<GameObject> myStreamRX = Stream.of(this.npcItems.xObjects);
+        this.showItems = myStreamRX
+                .distinct()
+                .filter(x -> {if(x == Game.emptyCell){
+                    return false;
+                } else return true;
+                })
+                .collect(Collectors.toList()).toArray(new GameObject[0]);
     }
 
 }
