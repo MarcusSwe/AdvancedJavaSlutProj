e7e0e8bfa45fe7c99a9e589bb433f833fbe4d778
MarcusSwe <69968155+MarcusSwe@users.noreply.github.com>
2021-02-12 16:05:13 +0100

    Delete Container.java

--- Container.java
+++
@@ -1,8 +0,0 @@
-import java.io.Serializable;
-
-public class Container extends GameObject implements Serializable {
-
-    public Container(String y, Boolean x, Boolean z) {
-        super(y, x, z);
-    }
-}
